dd0dc9751180fbacafb7027c9a2a740bebdade3f
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2025-03-28 14:32:57 -0500

    Implementación del Coyote Time

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,37 @@
 extends CharacterBody2D
 const SPEED = 150
 const JUMP_VELOCITY = -300.0
+#Variables
+var coyoteTime:bool=true
 #Importaciones
 @onready var  animatedSprite2D=$AnimatedSprite2D
+@onready var  coyoteTimer=$CoyoteTimer
+
+func _on_coyote_timer_timeout() -> void:
+	coyoteTime=false
+	print("El valor del coyote time al acabar el timer es ", coyoteTime)
+
 
 #Funciones
 func _physics_process(delta: float) -> void:
+	#Coyote Time
+	if is_on_floor():
+		coyoteTime=true
+		print ("El valor de coyote time al pisar el suelo es ", coyoteTime)
+	elif (not is_on_floor() and coyoteTimer.time_left<=0):
+		coyoteTimer.start()
+		
+
+	
+	
+	
+	
 	#Caída
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	#Salto
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and (is_on_floor() or coyoteTime==true):
+		coyoteTime=false
 		velocity.y = JUMP_VELOCITY
 	#Animaciones
 	var direction := Input.get_axis("move_left", "move_right")
