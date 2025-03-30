f595fa133955be8d36b54ec7fd6d4a0825d88bc0
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2025-03-30 07:56:08 -0500

    Refactorización del código

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,21 +3,41 @@ const SPEED = 150
 const JUMP_VELOCITY = -300.0
 #Variables
 var coyoteTime:bool=true
+@export var attacking:bool=false
 #Importaciones
 @onready var  animatedSprite2D=$AnimatedSprite2D
+@onready var  animationPlayer=$AnimationPlayer
 @onready var  coyoteTimer=$CoyoteTimer
+
 
 func _on_coyote_timer_timeout() -> void:
 	coyoteTime=false
-	print("El valor del coyote time al acabar el timer es ", coyoteTime)
+
+func animations()->void:
+	var direction := Input.get_axis("move_left", "move_right")
+	if (direction<0):
+		animatedSprite2D.flip_h=true
+	if (direction>0):
+		animatedSprite2D.flip_h=false
+	if is_on_floor():
+		if(attacking):
+			animationPlayer.play("attack")
+		elif (direction==0):
+			animatedSprite2D.play("default")
+		else:
+			animatedSprite2D.play("run")
+	else:
+		animatedSprite2D.play("jump")
 
 
-#Funciones
-func _physics_process(delta: float) -> void:
+func attack():
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		attacking=true
+
+func jump(delta):
 	#Coyote Time
 	if is_on_floor():
 		coyoteTime=true
-		print ("El valor de coyote time al pisar el suelo es ", coyoteTime)
 	elif (not is_on_floor() and coyoteTimer.time_left<=0):
 		coyoteTimer.start()
 	#Caída
@@ -27,22 +47,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Jump") and (is_on_floor() or coyoteTime==true):
 		coyoteTime=false
 		velocity.y = JUMP_VELOCITY
-	#Animaciones
+	
+func movement():
 	var direction := Input.get_axis("move_left", "move_right")
-	if (direction<0):
-		animatedSprite2D.flip_h=true
-	if (direction>0):
-		animatedSprite2D.flip_h=false
-	if is_on_floor():
-		if (direction==0):
-			animatedSprite2D.play("default")
-		else:
-			animatedSprite2D.play("run")
-	else:
-		animatedSprite2D.play("jump")
-	#Movimiento
-	if direction:
+	if direction and !attacking:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+#Funciones
+func _physics_process(delta: float) -> void:
+	movement()
+	jump(delta)
+	attack()
+	animations()
 	move_and_slide()
